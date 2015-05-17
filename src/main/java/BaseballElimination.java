7749c84cc221ab542aa5d41a3f9f993e55c401ce
tryharder <maiorov.prog@gmail.com>
2015-05-17 21:00:41 +0300

    * implmented parsing of input data

--- src/main/java/BaseballElimination.java
+++ src/main/java/BaseballElimination.java
@@ -1,32 +1,80 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseballElimination {
+    private final int NAME = 0;
+    private final int WINS = 1;
+    private final int LOSSES = 2;
+    private final int LEFT = 3;
+    private final int SCHEDULE_PARSE_POSITION = 4;
+
+    private Map<String, TeamInfo> statistic = new HashMap<String, TeamInfo>();
+
+    private int numberOfTeams;
 
     public BaseballElimination(String filename) {
+        processInputFile(filename);
+    }
+
+    private void processInputFile(String filename) {
         In in = new In(filename);
+        int id = 0;
+        if (in.hasNextLine())
+            numberOfTeams = Integer.parseInt(in.readLine());
+        while(in.hasNextLine()) {
+            String line = in.readLine();
+            createTeamInfoAndPutItToMap(line, id++);
+        }
+    }
+
+    private void createTeamInfoAndPutItToMap(String line, int id) {
+        String[] result = line.split("\\s+");
+
+        TeamInfo teamInfo = new TeamInfo();
+        teamInfo.setWins(Integer.parseInt(result[WINS]));
+        teamInfo.setTeamName(result[NAME]);
+        teamInfo.setLosses(Integer.parseInt(result[LOSSES]));
+        teamInfo.setLeft(Integer.parseInt(result[LEFT]));
+        teamInfo.setSchedule(parseSchedule(result));
+        teamInfo.setId(id);
+
+        statistic.put(result[NAME], teamInfo);
+    }
+
+    private int[] parseSchedule(String[] result) {
+        int length =  result.length - SCHEDULE_PARSE_POSITION;
+        int[] schedule = new int[length];
+
+        for (int i = SCHEDULE_PARSE_POSITION; i < result.length; i++) {
+            schedule[i-SCHEDULE_PARSE_POSITION] = Integer.parseInt(result[i]);
+        }
+
+        return schedule;
     }
 
     public int numberOfTeams() {
-        return 0;
+        return numberOfTeams;
     }
 
     public Iterable<String> teams() {
-        return null;
+        return statistic.keySet();
     }
 
     public int wins(String team) {
-        return 0;
+        return statistic.get(team).getWins();
     }
 
     public int losses(String team) {
-        return 0;
+        return statistic.get(team).losses;
     }
 
     public int remaining(String team) {
-        return 0;
+        return statistic.get(team).left;
     }
 
     public int against(String team1, String team2) {
-        return 0;
+        int enemyId = statistic.get(team2).getId();
+        return statistic.get(team1).getSchedule()[enemyId];
     }
 
     public boolean isEliminated(String team) {
@@ -35,5 +83,72 @@ public class BaseballElimination {
 
     public Iterable<String> certificateOfElimination(String team) {
         return null;
+    }
+
+    private class TeamInfo {
+        private String teamName;
+        private Integer wins;
+        private Integer losses;
+        private Integer left;
+        private int[] schedule;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        private int id;
+
+        public int[] getSchedule() {
+            return schedule;
+        }
+
+        public void setSchedule(int[] schedule) {
+            this.schedule = schedule;
+        }
+
+        public String getTeamName() {
+            return teamName;
+        }
+
+        public void setTeamName(String teamName) {
+            this.teamName = teamName;
+        }
+
+        public Integer getWins() {
+            return wins;
+        }
+
+        public void setWins(Integer wins) {
+            this.wins = wins;
+        }
+
+        public Integer getLosses() {
+            return losses;
+        }
+
+        public void setLosses(Integer losses) {
+            this.losses = losses;
+        }
+
+        public Integer getLeft() {
+            return left;
+        }
+
+        public void setLeft(Integer left) {
+            this.left = left;
+        }
+    }
+
+    public static void main(String[] args) {
+        BaseballElimination baseball = new BaseballElimination("teams4.txt");
+        System.out.println(baseball.wins("Atlanta"));
+        System.out.println(baseball.losses("Philadelphia"));
+        System.out.println(baseball.remaining("New_York"));
+        System.out.println(baseball.numberOfTeams());
+        System.out.println(baseball.against("Atlanta", "Montreal"));
     }
 }
