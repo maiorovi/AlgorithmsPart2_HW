e8e395214a63c6148acbc855a190cd8e60187523
tryharder <maiorov.prog@gmail.com>
2015-05-24 14:51:50 +0300

    * created two classes to start fifth week task

---
+++ src/main/java/BoggleBoard.java
@@ -0,0 +1,45 @@
+public class BoggleBoard {
+    // Initializes a random 4-by-4 Boggle board.
+    // (by rolling the Hasbro dice)
+    public BoggleBoard() {
+
+    }
+
+    // Initializes a random M-by-N Boggle board.
+    // (using the frequency of letters in the English language)
+    public BoggleBoard(int M, int N) {
+
+    }
+
+    // Initializes a Boggle board from the specified filename.
+    public BoggleBoard(String filename) {
+
+    }
+
+    // Initializes a Boggle board from the 2d char array.
+    // (with 'Q' representing the two-letter sequence "Qu")
+    public BoggleBoard(char[][] a) {
+
+    }
+
+    // Returns the number of rows.
+    public int rows() {
+        return 0;
+    }
+
+    // Returns the number of columns.
+    public int cols() {
+        return 0;
+    }
+
+    // Returns the letter in row i and column j.
+    // (with 'Q' representing the two-letter sequence "Qu")
+    public char getLetter(int i, int j) {
+        return 'a';
+    }
+
+    // Returns a string representation of the board.
+    public String toString() {
+        return null;
+    }
+}
